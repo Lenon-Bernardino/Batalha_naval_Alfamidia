7deff8ab1244566815f507e6e8f28335c56b24f5
Lenon Bernardino <lenon.bernardino.aluno@alvorada.ifrs.edu.br>
2022-07-03 19:00:39 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void Main()
+    {
+        int x = 0;
+        int y = 0;
+        String Command = "";
+        Scanner Scanner1 = new Scanner(System.in);
+        
+        while(true)
+        {
+            System.out.println("========================================");
+            System.out.println("Write x,y to input a coordinate");
+            System.out.println("Write end to end the game");
+            System.out.println("========================================\n\n");
+            
+            System.out.println("Command: ");
+            Command = Scanner1.nextLine();
+            
+            if(Command == "end")
+            {
+                break;
+            }
+            if(Command.contains(","))
+            {
+                try
+                {
+                    String[] variables = Command.split(",");
+                    String StringX = variables[0];
+                    String StringY = variables[1];
+                    x = Integer.parseInt(StringX);
+                    y = Integer.parseInt(StringY);
+                    System.out.println("You've inputted: " + x + ", " + y);
+                }
+                catch (NumberFormatException ex)
+                {
+                    System.out.println("Invalid input, you must input two integers x and y in the format x,y, neither bigger than 10\n");
+                }
+            }
+        }
+    }
+}
